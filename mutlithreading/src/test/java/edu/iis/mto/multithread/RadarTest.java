f53207044c632241335e078951eaec0592105b87
Krzysztof Chybicki <krysiek97@interia.eu>
2020-05-15 15:06:39 +0200

    modified verify to work with timeout

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,19 +1,18 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import org.junit.jupiter.api.RepeatedTest;
+
+import static org.mockito.Mockito.*;
 
 public class RadarTest {
 
     @RepeatedTest(10)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
+        Radar radar = new Radar(batteryMock, 1);
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+        verify(batteryMock, timeout(100)).launchPatriot(enemyMissle);
     }
 
 }
