1adc975f0583e7d9f85e6d41e65d46a6c5e8aaf5
Asmei <kamsykula@gmail.com>
2020-05-16 14:23:39 +0200

    Added BetterRadar

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.multithread;
+
+public class BetterRadar {
+	private PatriotBattery battery;
+	
+	public BetterRadar(PatriotBattery battery) {
+		this.battery = battery;
+	}
+	
+	public void notice(Scud enemyMissle) {
+	
+	}
+}
