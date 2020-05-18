6e46f409477133073eea4ca84ca1234e99517083
Patryk Janowski <pjanowski97@gmail.com>
2020-05-18 22:17:07 +0200

    typo in name corrected

--- mutlithreading/src/main/java/edu/iis/mto/multithread/BatterySystem.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BatterySystem.java
@@ -2,6 +2,6 @@ package edu.iis.mto.multithread;
 
 public interface BatterySystem {
 
-    void launchPatriot(PatriotBattery patriotBattery, Scud enemyMissle, int numberOfMissiles);
+    void launchPatriot(PatriotBattery patriotBattery, Scud enemyMissile, int numberOfMissiles);
 
 }
