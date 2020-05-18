0d91593bddc939c39dfe79d239565404b9b3e06a
Przemysław Mirowski <miretpl@gmail.com>
2020-05-18 15:49:15 +0200

    add repeated test addnotation

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,11 +3,11 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 public class RadarTest {
 
-    @Test
+    @RepeatedTest(10)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
