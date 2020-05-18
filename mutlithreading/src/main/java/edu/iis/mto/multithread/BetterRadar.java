53ea82b875ee575c6177a0c36ac4b7241b0d2392
Patryk Janowski <pjanowski97@gmail.com>
2020-05-18 22:17:34 +0200

    BetterRadar class added

--- mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,4 +1,19 @@
 package edu.iis.mto.multithread;
 
 public class BetterRadar {
+
+    private PatriotBattery battery;
+    private BatterySystem batterySystem;
+    private int numberOfMissiles;
+
+    public BetterRadar(PatriotBattery battery, BatterySystem batterySystem, int numberOfMissiles) {
+        this.battery = battery;
+        this.batterySystem = batterySystem;
+        this.numberOfMissiles = numberOfMissiles;
+    }
+
+    public void notice(Scud enemyMissile) {
+        this.batterySystem.launchPatriot(this.battery, enemyMissile, this.numberOfMissiles);
+    }
+
 }
