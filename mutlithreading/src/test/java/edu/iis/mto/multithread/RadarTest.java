51536205d9d675489c60e7347752062f252af973
Przemysław Mirowski <miretpl@gmail.com>
2020-05-18 20:49:53 +0200

    modify radarTest class

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,19 +1,28 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+
+import static org.mockito.Mockito.*;
 
 public class RadarTest {
 
+    PatriotBattery batteryMock;
+    TargetMechanism targetMechanismMock;
+    Scud enemyMissile;
+
+    @BeforeEach
+    void setUp() {
+        batteryMock = mock(PatriotBattery.class);
+        targetMechanismMock = mock(TargetMechanism.class);
+        enemyMissile = new Scud();
+    }
+
     @RepeatedTest(10)
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+    public void launchPatriotOnceWhenNoticesAScudMissile() {
+        BetterRadar betterRadar = new BetterRadar(batteryMock, targetMechanismMock, 1);
+        betterRadar.notice(enemyMissile);
+        verify(targetMechanismMock).launchPatriot(enemyMissile, 1);
     }
 
 }
