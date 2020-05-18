a086d4b9c862c2b7f3cb5b2930b6286e78da8cbe
Patryk Janowski <pjanowski97@gmail.com>
2020-05-18 22:28:15 +0200

    Test adjusted to use BetterRadar class

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,17 +3,32 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 
 public class RadarTest {
 
-    @RepeatedTest(10)
+    private PatriotBattery batteryMock;
+    private BatterySystem batterySystemMock;
+    private Scud enemyMissile;
+    private int numberOfMissiles;
+
+    private BetterRadar betterRadar;
+
+    @BeforeEach
+    public void setUp() {
+        this.batteryMock = mock(PatriotBattery.class);
+        this.batterySystemMock = mock(BatterySystem.class);
+        this.enemyMissile = mock(Scud.class);
+
+        this.numberOfMissiles = 5;
+        this.betterRadar = new BetterRadar(this.batteryMock, this.batterySystemMock, this.numberOfMissiles);
+    }
+
+    @RepeatedTest(7)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+        this.betterRadar.notice(this.enemyMissile);
+        verify(this.batterySystemMock).launchPatriot(this.batteryMock, this.enemyMissile, this.numberOfMissiles);
     }
 
 }
