72ec5278e4fec1078ccf57ecd97f80794fd275f3
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-19 19:09:52 +0200

    Extracted batteryMock and enemyMissile from methods, to initialize them in @BeforeEach annotated method prepareNeededMocksAndData

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -10,43 +10,42 @@ import static org.mockito.Mockito.*;
 public class RadarTest {
 
     private DefenseSystem system;
+    private PatriotBattery batteryMock;
+    private Scud enemyMissile;
 
     @BeforeEach
-    void prepareDefenseSystemMock() {
+    void prepareNeededMocksAndData() {
         system = mock(DefenseSystem.class);
         doAnswer(invocationOnMock -> {
             ((Runnable) invocationOnMock.getArgument(0)).run();
             return null;
         }).when(system)
           .executeDefenseCommand(any(Runnable.class));
+
+        batteryMock = mock(PatriotBattery.class);
+        enemyMissile = new Scud();
     }
 
     @RepeatedTest(10)
     public void launchPatriotOnceWhenNoticesAScudMissile() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock, 1);
         radar.setDefenseSystem(system);
-        Scud enemyMissile = new Scud();
         radar.notice(enemyMissile);
         verify(batteryMock, times(1)).launchPatriot(enemyMissile);
     }
 
     @RepeatedTest(10)
     public void doNotLaunchPatriotWhenNoticesAScudMissileIfThereIsTheLackOfPatriotMissiles() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock, 0);
         radar.setDefenseSystem(system);
-        Scud enemyMissile = new Scud();
         radar.notice(enemyMissile);
         verify(batteryMock, times(0)).launchPatriot(enemyMissile);
     }
 
     @Test
     public void applyingNullDefenseSystemShouldCauseNPE(){
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock, 1);
         radar.setDefenseSystem(null);
-        Scud enemyMissile = new Scud();
         assertThrows(NullPointerException.class, ()-> radar.notice(enemyMissile));
     }
 
