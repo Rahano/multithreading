cfd7d308445354cf06889034e5feba95b8cfacf9
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-19 17:08:25 +0200

    Prepared DefenseSystem mock and its initialization and behaviour setup before each test.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,16 +1,26 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
 
 public class RadarTest {
 
+    private DefenseSystem system;
+
+    @BeforeEach
+    void prepareDefenseSystemMock(){
+        system = mock(DefenseSystem.class);
+        doAnswer(invocationOnMock -> {((Runnable)invocationOnMock.getArgument(0)).run(); return null;})
+                .when(system)
+                .executeDefenseCommand(any(Runnable.class));
+    }
     @Test
+
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
+        BetterRadar radar = new BetterRadar(batteryMock, 1);
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
         verify(batteryMock).launchPatriot(enemyMissle);
