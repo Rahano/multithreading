54988ac521c0c61cf3645af2dbb6ad50206e20f7
Hergoln <juliusz.malka@o2.pl>
2020-05-21 12:19:37 +0200

    zmieniono nazwę testu launchPatriotOnceWhenNoticesAScudMissle oraz dodano test launchPatriotFourTimesWhenNoticesAScudMissileAndVolleySizeIs4

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -2,20 +2,28 @@ package edu.iis.mto.multithread;
 
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class BetterRadarTest {
 
     @Test
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
+    public void launchPatriotOnceWhenNoticesAScudMissileAndVolleySizeIs1() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
-        BetterRadar radar = new BetterRadar(batteryMock, 1);
+        ControlSystem controlSystem = new TestControlSystem();
+        BetterRadar radar = new BetterRadar(controlSystem, batteryMock, 1);
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
         verify(batteryMock).launchPatriot(enemyMissle);
     }
 
-
-
+    @Test
+    public void launchPatriotFourTimesWhenNoticesAScudMissileAndVolleySizeIs4() {
+        PatriotBattery batteryMock = mock(PatriotBattery.class);
+        ControlSystem controlSystem = new TestControlSystem();
+        int volleySize = 4;
+        BetterRadar radar = new BetterRadar(controlSystem, batteryMock, volleySize);
+        Scud enemyMissle = new Scud();
+        radar.notice(enemyMissle);
+        verify(batteryMock, times(volleySize)).launchPatriot(enemyMissle);
+    }
 }
