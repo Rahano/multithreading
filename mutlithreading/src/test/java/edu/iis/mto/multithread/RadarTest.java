d9c2a480af6f075557d14f3cba7fe00758d5f251
Michal <michalogloza97@gmail.com>
2020-05-25 16:18:13 +0200

    Added test fixture.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,17 +3,26 @@ package edu.iis.mto.multithread;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class RadarTest {
 
+    private PatriotBattery batteryMock;
+    private Scud enemyMissile;
+
+    @BeforeEach
+    public void init()
+    {
+        batteryMock = mock(PatriotBattery.class);
+        enemyMissile = new Scud();
+    }
+
     @RepeatedTest(10)
     public void launchPatriotZeroTimesWhenNoticesAScudMissile() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         LaunchPatriotTask launchTask = new LaunchPatriotTask(batteryMock, 0);
         BetterRadar radar = new BetterRadar(launchTask);
-        Scud enemyMissile = new Scud();
         radar.notice(enemyMissile);
         try {
             launchTask.join();
@@ -25,10 +34,8 @@ public class RadarTest {
 
     @RepeatedTest(10)
     public void launchPatriotOnceWhenNoticesAScudMissile() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         LaunchPatriotTask launchTask = new LaunchPatriotTask(batteryMock, 1);
         BetterRadar radar = new BetterRadar(launchTask);
-        Scud enemyMissile = new Scud();
         radar.notice(enemyMissile);
         try {
             launchTask.join();
