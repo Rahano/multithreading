875f55517233d070cf9478fd0000cca2384f9c1a
Michal <michalogloza97@gmail.com>
2020-05-25 16:13:25 +0200

    Added launchPatriotZeroTimesWhenNoticesAScudMissile test.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,14 +1,27 @@
 package edu.iis.mto.multithread;
 
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class RadarTest {
 
+    @RepeatedTest(10)
+    public void launchPatriotZeroTimesWhenNoticesAScudMissile() {
+        PatriotBattery batteryMock = mock(PatriotBattery.class);
+        LaunchPatriotTask launchTask = new LaunchPatriotTask(batteryMock, 0);
+        BetterRadar radar = new BetterRadar(launchTask);
+        Scud enemyMissile = new Scud();
+        radar.notice(enemyMissile);
+        try {
+            launchTask.join();
+        } catch (InterruptedException e){
+            fail("Interrupted");
+        };
+        verify(batteryMock, times(0)).launchPatriot(enemyMissile);
+    }
 
     @RepeatedTest(10)
     public void launchPatriotOnceWhenNoticesAScudMissile() {
