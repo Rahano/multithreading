30af1ba76d826c4cd0ba2ff0d98982d1c38e5ae6
belka <izaa1109@gmail.com>
2020-06-16 15:05:04 +0200

    added multithreading to LaunchPatrioticMock

--- mutlithreading/src/test/java/edu/iis/mto/multithread/LaunchPatrioticMock.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/LaunchPatrioticMock.java
@@ -3,7 +3,6 @@ package edu.iis.mto.multithread;
 import java.util.stream.IntStream;
 
 public class LaunchPatrioticMock implements LaunchPatriotic {
-	private Scud enemyMissle;
 	private PatriotBattery battery;
 	private int numberOfMissles = 0;
 
@@ -13,11 +12,8 @@ public class LaunchPatrioticMock implements LaunchPatriotic {
 
 	@Override
 	public void launchPatriot(PatriotBattery battery, Scud enemyMissle) {
-		if(this.battery == null || this.enemyMissle == null){
-			this.enemyMissle = enemyMissle;
-			this.battery = battery;
-			IntStream.range(0, numberOfMissles).forEach(i -> battery.launchPatriot(enemyMissle));
-		}
-
+		if(this.battery == null) this.battery = battery;
+		Runnable task = () -> IntStream.range(0, numberOfMissles).forEach(i -> battery.launchPatriot(enemyMissle));
+		task.run();
 	}
 }
